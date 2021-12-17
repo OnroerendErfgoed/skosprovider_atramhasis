88db0a1c75bdc21ebc410857293ad36e87dac8e2
Bram Goessens <bram.goessens@vlaanderen.be>
2021-12-17 11:45:24 +0100

    update copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'skosprovider_atramhasis'
-copyright = u'2015-2019, Flanders Heritage Agency'
+copyright = u'2015-2021, Flanders Heritage Agency'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
